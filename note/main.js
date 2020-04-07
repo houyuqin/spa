ae3741728ab93c217f5bfaf9a37cbe787fa68dec
houyuqin <1738519809@qq.com>
2020-04-07 18:21:38 +0800

    note

---
+++ note/main.js
@@ -0,0 +1,111 @@
+$(function(){
+  var $menu = $(''
+      +'<div id="menu">'
+          +'<div id="files">文件(F)</div>'
+          +'<div id="edit">编辑(E)</div>'
+          +'<div id="ooo">格式(O)</div>'
+          +'<div id="view">查看(V)</div>'
+          +'<div id="help">帮助(H)</div>'
+      +'</div>'
+      +'');
+    var $select = $(''
+     +'<div class="notepad-dlg-mask notepad-dlg-font">'
+      +'<div class="dialogbox notepad-dlgbox">'
+          +'<div class="notepad-dlg-titlebar">'
+              +'<p class="title">字体</p>'
+              +'<span class="close-btn">✖</span>'
+          +'</div>'
+          +'<div class="main notepad-dlg-main">'
+              +'<div class="font-family font-daxiao">'
+            +'<p>字体(F):</p>' 
+            +'<input type="text" class="one" value="宋体">'
+            +'<div id="family">'
+              + '<span style="font-family:幼圆">幼圆</span><br>'
+              + '<span style="font-family:新宋体">新宋体</span><br>'
+              + '<span style="font-family:微软雅黑">微软雅黑</span><br>'
+              + '<span style="font-family:宋体">宋体</span><br>'
+              + '<span style="font-family:隶书">隶书</span><br>'
+              + '<span style="font-family:楷体">楷体</span><br>'
+              + '<span style="font-family:华文中宋">华文中宋</span><br>'
+              + '<span style="font-family:黑体">黑体</span><br>'
+              + '<span style="font-family:仿宋">仿宋</span><br>'
+              + '<span style="font-family:方正粗黑宋简体">方正粗黑宋简体</span><br>'
+            +'</div>'
+          +'</div>'
+          +'<div class="font-style font-daxiao">'
+            +'<p>字形(Y):</p>'
+            +'<input type="text" class="two" value="常规">'
+               + '<div id="style">'
+                + '<span style="font-style:normal">常规</span><br>'
+                + '<span style="font-style:oblique">斜体</span><br>'
+                + '<span style="font-weight:bold">粗体</span><br>'
+                + '<span style="font-style:oblique;font-weight:bold">粗偏斜体</span><br>'
+              + '</div>'
+          +'</div>'
+          +'<div class="font-size font-daxiao">'
+            +'<p>大小(S):</p>'
+            +'<input type="text" class="three" value="14">'
+              + '<div id="size">'
+              + '<span>8</span><br>'
+              + '<span>9</span><br>'
+              + '<span>10</span><br>'
+              + '<span>11</span><br>'
+              + '<span>12</span><br>'
+              + '<span>14</span><br>'
+              + '<span>16</span><br>'
+              + '<span>18</span><br>'
+              + '<span>20</span><br>'
+              + '<span>24</span><br>'
+              + '<span>26</span><br>'
+              + '<span>28</span><br>'
+              + '<span>36</span><br>'
+              + '<span>48</span><br>'
+              + '<span>72</span><br>'
+              + '<span>初号</span><br>'
+              + '<span>小初</span><br>'
+              + '<span>一号</span><br>'
+              + '<span>二号</span><br>'
+              + '<span>小二</span><br>'
+            + '</div>'
+          +'</div>'
+              +'<br>'
+              +'<div class="list1"></div>'
+              +'<div class="list2"></div>'
+              +'<div class="list3"></div>'
+              +'<fieldset class="sample">'
+                +'<legend>示例</legend>'
+                +'<p id="egg">AaBbYyZz</p>'
+              +'</fieldset>'
+              +'<div class="script">'
+                +'<label>'
+                  +'脚本(R):<br>'
+                  +'<select>'
+                    +'<option value="西欧语言">西欧语言</option>'
+                    +'<option value="中文 GB2312">中文 GB2312</option>'
+                  +'</select>'
+                +'</label>'
+                +'</div>' 
+                +'<input class="btn-ok btn" type="button" value="确定">'
+                +'<input class="btn-cancel btn" id="cancel" type="button" value="取消">'
+            +'</div>'
+        +'</div>'
+     +'</div>'   
+    +'');
+
+  $('#box').append($menu);
+  var $ooo = $('#ooo'),
+      $cancel = $('#cancel');
+  $ooo.click(function(){
+    console.log(123);
+    $('#box').append($select); 
+  });
+  $cancel.click(function(){
+    console.log(555);
+    $('#box').remove($select);
+  });
+
+
+
+
+ 
+}());
